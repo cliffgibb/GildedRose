f1b576a05ebec974f97ed352b49ad5b1034aef6e
Cliff Fenwick Gibb <cliffgibb@gmail.com>
2020-05-21 10:54:40 -0300

    Hotfix for important thing

--- src/main/java/com/miwtech/gildedrose/controller/FutureFunctionController.java
+++ src/main/java/com/miwtech/gildedrose/controller/FutureFunctionController.java
@@ -3,5 +3,9 @@ package com.miwtech.gildedrose.controller;
 public class FutureFunctionController {
     public void doStuff() {
         System.out.println("Stuff done.");
+
+        String hello = "World";
+
+        System.out.println(" More Stuff done." + hello);
     }
 }
